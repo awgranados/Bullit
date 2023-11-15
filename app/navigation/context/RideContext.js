b18133619cfa51c11f2000eba91f008312915668
Hao <hao_wu@umail.ucsb.edu>
2023-11-14 21:30:54 -0800

    hw - setup database for creating ride requests/offers

--- app/navigation/context/RideContext.js
+++ app/navigation/context/RideContext.js
@@ -1,4 +1,6 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import { firestore } from '../../app/firebaseConfig';
+import { collection, onSnapshot, addDoc } from "firebase/firestore";
 
 const RideContext = createContext(null);
 
@@ -6,13 +8,41 @@ export const RideProvider = ({ children }) => {
   const [rideOffers, setRideOffers] = useState([]);
   const [rideRequests, setRideRequests] = useState([]);
 
-  const addRideOffer = (offer) => {
-    setRideOffers(prevOffers => [...prevOffers, offer]);
+  // Function to add a ride offer to Firestore
+  const addRideOffer = async (offer) => {
+    await addDoc(collection(firestore, 'rideOffers'), offer);
   };
 
-  const addRideRequest = (request) => {
-    setRideRequests(prevRequests => [...prevRequests, request]);
+  // Function to add a ride request to Firestore
+  const addRideRequest = async (request) => {
+    await addDoc(collection(firestore, 'rideRequests'), request);
   };
+
+  // Fetch ride offers from Firestore
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(firestore, 'rideOffers'), (snapshot) => {
+      const newOffers = snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setRideOffers(newOffers);
+    });
+  
+    return () => unsubscribe();
+  }, []);
+  
+  // Fetch ride requests from Firestore
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(firestore, 'rideRequests'), (snapshot) => {
+      const newRequests = snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setRideRequests(newRequests);
+    });
+  
+    return () => unsubscribe();
+  }, []);
 
   return (
     <RideContext.Provider value={{ rideOffers, rideRequests, addRideOffer, addRideRequest }}>
