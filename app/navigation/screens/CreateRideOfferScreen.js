cfe7f3f44f500069cd8d6a946b63ec4a303625c3
Hao <hao_wu@umail.ucsb.edu>
2023-11-17 17:19:52 -0800

    hw - modified the input fields of creating ride offers

--- app/navigation/screens/CreateRideOfferScreen.js
+++ app/navigation/screens/CreateRideOfferScreen.js
@@ -1,9 +1,9 @@
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, Alert, TextInput, StyleSheet } from 'react-native'
 import * as React from 'react';
-import { TextInput } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native'; 
 import { CreateButton } from 'app/app/button';
 import RideContext from '../context/RideContext';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 const CreateRideOfferScreen = () => {
     const [dest, setDest] = React.useState("");
@@ -14,7 +14,20 @@ const CreateRideOfferScreen = () => {
 
     const { addRideOffer } = React.useContext(RideContext);
 
+    const UCSB = "University of California, Santa Barbara, Santa Barbara, CA, USA";
+
     const handleDone = () => {
+      const fuelPriceNum = parseFloat(fuel_price);
+      if (isNaN(fuelPriceNum) || fuelPriceNum < 0 || fuelPriceNum > 1000) {
+          Alert.alert("Invalid Input", "Please enter a fuel price between 0.00 and 1000.00");
+          return;
+      }
+
+      if (departure !== UCSB) {
+        Alert.alert("Invalid Input", "We currently only allow rides depature from UCSB.");
+        return; // Return early to prevent further execution
+      }
+
       addRideOffer({ departure: departure, destination: dest, fuelPrice: fuel_price });
       navigation.navigate('Passenger'); 
     };
@@ -28,26 +41,60 @@ const CreateRideOfferScreen = () => {
     }, [navigation]); 
 
     return (
-    <View style={{ padding: 16 }}>
-      <TextInput
-        label="Departure"
-        value={departure}
-        onChangeText={(departure) => setDeparture(departure)}
-        mode="outlined"
-      />
+    <View style={{padding:16, flexDirection:'column', gap:10}}>
+      <GooglePlacesAutocomplete
+            placeholder='Departure'
+            fetchDetails={true}
+            onPress={(data, details = null) => {
+                // Set departure to "University of California, Santa Barbara" if it matches
+                if (data.description === "University of California, Santa Barbara, Santa Barbara, CA, USA") {
+                    setDeparture(data.description);
+                }
+            }}
+            query={{
+                key: 'AIzaSyD5s29CI_yIZQyR2TsfucfAtVpZCLMINcs',
+                language: 'en',
+                components: "country:us",
+                types:'establishment'
+            }}
+            styles={{
+                container: { flex: 0, width: "100%", zIndex: 1 },
+                listView: { backgroundColor: "black" },
+                textInput: styles.input // Apply your custom input style
+            }}
+            onFail={(error) => console.error(error)}
+            filterReverseGeocodingByTypes={['establishment']}
+            predefinedPlaces={[{
+                description: 'University of California, Santa Barbara, Santa Barbara, CA, USA',
+                geometry: { location: { lat: 34.4133, lng: -119.8610 } }
+            }]}
+        />
+
+      <GooglePlacesAutocomplete
+          placeholder='Destination'
+          fetchDetails={true}
+          onPress={(data, details = null) => {
+            setDest(data.description);
+          }}
+          query={{
+            key: 'AIzaSyD5s29CI_yIZQyR2TsfucfAtVpZCLMINcs',
+            language: 'en',
+            components: "country:us",
+            types: '(cities)' // restrict search to cities only
+          }}
+          styles={{
+            container: { flex: 0, width: "100%", zIndex: 1 },
+            listView: { backgroundColor: "black" },
+            textInput: styles.input
+          }}
+        />
 
       <TextInput
-        label="Destination"
-        value={dest}
-        onChangeText={dest => setDest(dest)}
-        mode='outlined'
-      />
-
-      <TextInput
-          label="Fuel Price"
-          value={fuel_price}
+          style={styles.input}
           onChangeText={fuel_price => setFuelPrice(fuel_price)}
-          mode='outlined'
+          value={fuel_price}
+          placeholder="Fuel Price"
+          keyboardType="numeric"
       />
 
       <View style={{ marginTop: 20 }}>
@@ -56,6 +103,20 @@ const CreateRideOfferScreen = () => {
 
       </View>
     );
-}
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 50,
+    marginVertical: 8,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: '#BDBDBD',
+    backgroundColor: '#ffffff',
+    borderRadius: 5,
+    fontSize: 16,
+    color: '#424242',
+  },
+});
 
 export default CreateRideOfferScreen
