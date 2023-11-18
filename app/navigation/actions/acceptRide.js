f376f4e726e955d854496803709e875ab90167ab
r_hinojosa <r_hinojosa@ucsb.edu>
2023-11-17 23:14:25 -0800

    bug fixes to acceptRide

--- app/navigation/actions/acceptRide.js
+++ app/navigation/actions/acceptRide.js
@@ -12,31 +12,37 @@ import {
 } from "firebase/firestore";
 
 function adjustSeatPrice(total, seatsReserved) {
-  return (total / seatsReserved).toFixed(2);
+  return Number((total / seatsReserved).toFixed(2));
 }
 
 export default async function acceptRide(docUID) {
   const user = auth.currentUser;
   if (user) {
-    rideOffer = await getDoc(docUID);
-    totalPrice = rideOffer.get("totalPrice");
-    newSeatsTaken = rideOffer.get("seatsTaken") + 1;
-    acceptedSeatPrice = rideOffer.get("seatPrice");
-    updatedRideOffer = await updateDoc(docUID, {
-      seatsTaken: newSeatsTaken,
-      seatPrice: adjustSeatPrice(totalPrice, newSeatsTaken),
-      pasengersUserUID: arrayUnion([user.uid]),
-    });
-    newSeatPrice = updatedRideOffer.get("seatPrice");
-    addDoc(
-      collection(firestore, "acceptedRidesPassenger", {
-        acceptedOn: Timestamp.now(),
-        pasengerUserUID: user.uid,
-        rideOffer: doc(firestore, "rideOffers/" + docUID),
-        rideCost: acceptedSeatPrice,
-        passengersAmount: newSeatsTaken,
-        newSeatPrice: newSeatPrice,
-      })
-    );
+    try {
+      rideOffer = await getDoc(doc(firestore, "rideOffers", docUID));
+      totalPrice = rideOffer.data().totalPrice;
+      newSeatsTakenForCalculation = rideOffer.data().seatsTaken + 2;
+      newSeatsTaken = rideOffer.data().seatsTaken + 1;
+      acceptedSeatPrice = rideOffer.data().seatPrice;
+      updateDoc(doc(firestore, "rideOffers", docUID), {
+        seatsTaken: newSeatsTaken,
+        seatPrice: adjustSeatPrice(totalPrice, newSeatsTakenForCalculation),
+        pasengersUserUID: arrayUnion(user.uid),
+      });
+      updatedRideOffer = await getDoc(doc(firestore, "rideOffers", docUID));
+      newSeatPrice = updatedRideOffer.data().seatPrice;
+      addDoc(
+        collection(firestore, "acceptedRidesPassenger", {
+          acceptedOn: Timestamp.now(),
+          pasengerUserUID: user.uid,
+          rideOffer: doc(firestore, "rideOffers/" + docUID),
+          rideCost: acceptedSeatPrice,
+          passengersAmount: newSeatsTaken,
+          newSeatPrice: newSeatPrice,
+        })
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 }
