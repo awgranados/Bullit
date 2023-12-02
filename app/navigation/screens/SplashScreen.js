3460693fbb9ae108cee93287ee4f8c3506938c89
adityapatil834 <aditya_patil@ucsb.edu>
2023-12-01 17:53:18 -0800

    changed fron lottie so It works w expo

--- app/navigation/screens/SplashScreen.js
+++ app/navigation/screens/SplashScreen.js
@@ -1,20 +1,37 @@
 import React, {Component, component } from "react";
-import { View, StyleSheet, Dimensions } from "react-native";
+import { View, Image, StyleSheet, Dimensions } from "react-native";
+import FastImage from 'react-native-fast-image';
 import { Animated, Easing } from "react-native";
 import LottieView from "lottie-react-native";
 
 export default class Splash extends Component {
     constructor(props) {
-        super();
+        super(props);
+        this.state = {
+          timer: null,
+        };
+    }
+
+    componentDidMount() {
+        // Set a timer for the GIF duration
+        this.setState({
+          timer: setTimeout(() => {
+            this.props.navigation.replace('Login');
+          }, 3000) // Set this to your GIF's duration
+        });
+    }
+
+    componentWillUnmount() {
+        // Clear the timer when the component is unmounted
+        if (this.state.timer) {
+          clearTimeout(this.state.timer);
+        }
     }
 
     render() {
         return (
-            <View style={{
-                flex: 1,
-                backgroundColor: '#002E5D',
-                }}
-            >
+            <View style={styles.container}>
+                {/* this has trouble on expo :
                 <LottieView
                     source={require('../../assets/Animation 1.json')}
                     autoPlay
@@ -27,6 +44,19 @@ export default class Splash extends Component {
                     onError={(error) => {
                       console.error("Lottie Error: ", error);
                     }}
+
+                    -----
+
+                    <Image
+                source={require('../../assets/Animation1.gif')} 
+                style={styles.image}
+                onLoadEnd={() => this.props.navigation.replace('Login')} // Navigate when the GIF is loaded
+              />
+                />
+                */}
+                <Image
+                    source={require('../../assets/Animation1.gif')} // Make sure the path is correct
+                    style={styles.image}
                 />
             </View>
         )
@@ -38,7 +68,12 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: '#002E5D',
         width: Dimensions.get('window').width, // Take up the full screen width
         height: Dimensions.get('window').height, // Take up the full screen height
+    },
+    image: {
+        width: 150, 
+        height: 150, 
     },
 });
