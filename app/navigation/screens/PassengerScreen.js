07e68f036108579fe059b9f52f251e61c8342b90
Alex Granados <alexg@MacBook-Air-2.local>
2023-12-07 00:53:03 -0800

    Added edit profile Modal

--- app/navigation/screens/PassengerScreen.js
+++ app/navigation/screens/PassengerScreen.js
@@ -1,43 +1,122 @@
 import * as React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import{ useState } from 'react';
+import {View, Text, StyleSheet, Modal, TextInput} from 'react-native';
 import {CreateButton, IconButton} from 'app/app/button';
 import { Avatar, Button, Card } from 'react-native-paper';
+import { doc, setDoc, Timestamp, GeoPoint, getDoc, data} from "firebase/firestore";
+import { firestore } from "../../app/firebaseConfig";
 
-export default function PassengerScreen({navigation}) {
-    return(
-        <View style={styles.container}>
-              <View style={{alignItems:'center'}}>
-                <Text
-                    style = {styles.text}>Passenger Screen
-                </Text>
-              </View>
+
+
+
+export default function ProfileScreen({navigation}) {
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [newDisplayName, setNewDisplayName] = useState('');
+  const user = auth.currentUser;
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+  
+  //style={{ textAlign: 'right', fontSize: 15, color:"black" }}>{user.email}
+  
+  //const userDocRef = doc(firestore, "userDetails", user.uid);
+  console.log(user);
+
+  
+  const signOut = () => {
+    if(user){
+     auth.signOut;
+      navigation.navigate("Login");
+    }
+  };
+
+  const updateDisplayName = () => {
+    // You should add validation for newDisplayName here if needed
+
+    // Update the user's display name
+    user.updateProfile({
+      displayName: newDisplayName,
+    })
+      .then(() => {
+        toggleModal(); // Close the modal after updating display name
+      })
+      .catch((error) => {
+        console.error('Error updating display name:', error.message);
+        // Handle error
+      });
+  };
+
+  return(
+      
+      <View style = {styles.container}>    
+
+          <Text style={styles.header}>{user.displayName}</Text>
+          <Text style = {styles.text}>{}</Text>                       
+          <CreateButton text='Edit Profile'onPress={toggleModal} />
+          <CreateButton text='Logout' onPress={signOut}/>
+          <Modal visible={isModalVisible} animationType="slide">
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalHeader}>Edit Display Name</Text>
+          <TextInput
+            style={styles.modalInput}
+            placeholder="Enter new display name"
+            //value={newDisplayName}
+          />
+          <CreateButton text="Save" onPress={toggleModal} />
+          <CreateButton text="Cancel" onPress={toggleModal} />
         </View>
-    );
-}
+      </Modal>
+      </View>
+  );
 
+  
+}
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 60,
-    flex: 1,
-    backgroundColor: "#FFFFFF",
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: "#FFFFFF",
+  },
+  header: {
+      fontSize: 35,
+      fontWeight: "bold", 
+      padding: 10, 
+      color: '#002E5D'
   },
   text: {
-    fontSize: 26,
-    fontWeight: "bold",
-    color: "#002E5D",
+      fontSize: 20,
+      fontWeight: "bold", 
+      padding: 3, 
+      color: '#002E5D'
   },
-  card: {
-    backgroundColor: "#002E5D",
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
   },
-  text2: {
+  modalHeader: {
     fontSize: 20,
-    fontWeight: "bold",
-    color: "#002E5D",
+    fontWeight: 'bold',
+    marginBottom: 20,
   },
-  text3: {
-    color: "#FFFFFF",
+  modalInput: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    padding: 8,
   },
-  button: {
-    backgroundColor: "#FF4618",
-  },
+  Button: {
+    borderRadius: 8,
+    paddingVertical: 14,
+    paddingHorizontal: 10,
+    backgroundColor: '#FF4618',
+    width: 250,
+    height: 50,
+    alignSelf: 'center'
+},
 });
